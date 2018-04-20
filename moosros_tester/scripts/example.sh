b1993c63516256ead4d81f6ec5c10336ffd5354c
Tony Hsiao <tonycar12002@gmail.com>
2018-04-20 12:56:35 -0400

    add ignore about log file and rename the file

--- moosros_tester/scripts/example.sh
+++ moosros_tester/scripts/example.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Launch the ROS nodes
-roslaunch moosros_tester counter.launch &
+roslaunch moosros_tester examle.launch &
 ROSLAUNCH_PID=$!
 
 # Launch the MOOS nodes
-pAntler $(rospack find moosros)/bridge.moos >& /dev/null &
+pAntler $(rospack find moosros)/moos/example/bridge.moos >& /dev/null &
 PANTLER_PID=$!
 
-uMAC $(rospack find moosros)/bridge.moos
+uMAC $(rospack find moosros)/moos/example/bridge.moos
 
 printf "Killing all processes, safely ... \n"
 kill -SIGTERM $ROSLAUNCH_PID
